adde74ecbfb0c99b47dfcd387ead1ad05b1d08af
Alex Efremov <alex.efremov.dev@gmail.com>
2015-12-29 23:48:44 +0200

    Change cursor to pointer and minor style improvements

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 $mancityInfo = $(".mancity-info");
 $dynamoInfo = $(".dynamo-info");
+$club = $(".club");
 
-$(".club").first().on("click", function() {
+$club.first().on("click", function() {
   $mancityInfo.slideUp();
   $dynamoInfo.slideToggle();
 });
 
-$(".club").eq(1).on("click", function() {
+$club.eq(1).on("click", function() {
   $dynamoInfo.slideUp();
   $mancityInfo.slideToggle();
 });
